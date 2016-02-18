c90ebb3c399723140b2741cfc000be15180e766a
MrMatEng <burakagachan@gmailcom>
2016-02-18 17:57:10 +0200

    Baslangic

---
+++ FirstStoreApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstStoreApplication
+//
+//  Created by Hazal Çengel on 16.02.2016.
+//  Copyright © 2016 Hazal Çengel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
